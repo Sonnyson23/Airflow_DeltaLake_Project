a26b5f256ea1edea0f0366b5127f37b7b411994b
Sonnyson23 <ademsonuvar@gmail.com>
2025-04-14 11:44:03 +0300

    retest

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,34 +1,28 @@
 from airflow import DAG
-from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
 from airflow.utils.email import send_email
-from airflow.models import TaskInstance
-from airflow.utils.state import State
+from datetime import datetime, timedelta
 
-
-start_date = datetime(2025, 4, 11)
-
-# callback fonksiyonu
 def notify_email_on_failure(context):
-    task_instance: TaskInstance = context.get('task_instance')
-    dag_run = context.get('dag_run')
-    log_url = task_instance.log_url  # UI'deki log linki
-    subject = f"[Airflow] Hata: {task_instance.task_id} görevi başarısız oldu!"
-    
+    task_instance = context.get("task_instance")
+    log_url = task_instance.log_url
+    subject = f"[Airflow] Hata: {task_instance.task_id} başarısız oldu!"
     html_content = f"""
-    <h3>Airflow Görev Hatası Bildirimi</h3>
+    <h3>Görev Hatası Bildirimi</h3>
     <p><strong>DAG:</strong> {task_instance.dag_id}</p>
     <p><strong>Görev:</strong> {task_instance.task_id}</p>
-    <p><strong>Yürütme Tarihi:</strong> {context.get('execution_date')}</p>
-    <p><strong>Hata Mesajı:</strong> {task_instance.try_number}. deneme başarısız.</p>
-    <p><strong>Loglar:</strong> <a href="{log_url}">Görev Logları</a></p>
+    <p><strong>Tarih:</strong> {context.get('execution_date')}</p>
+    <p><strong>Log:</strong> <a href="{log_url}">Görev Logları</a></p>
     """
-
-    send_email(to=["ademsonuvar@gmail.com"], subject=subject, html_content=html_content)
+    send_email(
+        to=["ademsonuvar@gmail.com"],
+        subject=subject,
+        html_content=html_content
+    )
 
 default_args = {
     'owner': 'airflow',
-    'start_date': start_date,
+    'start_date': datetime(2025, 4, 13),
     'retries': 1,
     'retry_delay': timedelta(seconds=5)
 }
@@ -36,12 +30,18 @@ default_args = {
 with DAG(
     'my_dag',
     default_args=default_args,
-    schedule_interval='@daily',
+    schedule_interval=None,
     catchup=False,
-    on_failure_callback=notify_email_on_failure
+    on_failure_callback=notify_email_on_failure  # <-- burada olmalı
 ) as dag:
-    
-    t0 = BashOperator(task_id='ls_data', bash_command='adem naber', retries=2, retry_delay=timedelta(seconds=15)) # 'ls -l /tmp'
+
+    t0 = BashOperator(
+        task_id='ls_data',
+        bash_command='adem naber',  # ← bilerek hatalı bırakılmış görev
+        retries=2,
+        retry_delay=timedelta(seconds=15)
+    )
+ # 'ls -l /tmp'
 
     t1 = BashOperator(task_id='download_data',
                       bash_command='curl -L -o /tmp/dirty_store_transactions.csv https://raw.githubusercontent.com/erkansirin78/datasets/refs/heads/master/churn-telecom/cell2celltrain.csv',
